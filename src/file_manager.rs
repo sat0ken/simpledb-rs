8701d5d9bc34096fc0444f8855a902c0ee2d66f5
sat0ken <15720506+sat0ken@users.noreply.github.com>
2024-11-11 10:17:38 +0900

    add files

---
+++ src/file_manager.rs
@@ -0,0 +1,39 @@
+pub struct BlockId {
+
+}
+
+impl BlockId {
+    pub fn block_id() {}
+    pub fn filename() {}
+    pub fn number() {}
+}
+
+pub struct Page {
+
+}
+
+impl Page {
+    pub fn page_int() {}
+    pub fn page_byte() {}
+    pub fn get_int() {}
+    pub fn get_byte() {}
+    pub fn get_string() {}
+    pub fn set_int() {}
+    pub fn set_byte() {}
+    pub fn set_string() {}
+    pub fn max_length() {}
+}
+
+pub struct FileManager {
+
+}
+
+impl FileManager {
+    pub fn file_manager() {}
+    pub fn read() {}
+    pub fn write() {}
+    pub fn append() {}
+    pub fn is_new() {}
+    pub fn length() {}
+    pub fn block_size() {}
+}
